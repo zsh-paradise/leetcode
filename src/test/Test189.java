386e15126ac239ce54829bfb644ac2875bafa4e6
zsh-paradise <297221517@qq.com>
2019-01-25 11:05:50 +0800

    修改

--- src/test/Test189.java
+++ src/test/Test189.java
@@ -4,10 +4,18 @@ package test;
  * Created by zsh_paradise on 2018/9/13.
  */
 public class Test189 {
-    public void rotate(int[] nums, int k) {
-        int temp = 0 ;
-        for (int i = 0; i <nums.length ; i++) {
+    public static int[] rotate(int[] nums, int k) {
+        k = k%nums.length;
+        int[] result = new  int[nums.length];
 
+        for (int i = 0; i <nums.length ; i++) {
+            result[(i+k)%nums.length] = nums[i];
         }
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate(nums,3);
     }
 }
